9c80e351d9db078628a0811d8a4d08c17cc7a1b2
Matthew Cebenka <matthewt.cebenka@gmail.com>
2019-11-08 13:35:05 -0500

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
-import Card from './components/Card'
+import CharacterCard from './components/Card';
 import "./App.css";
 
 const App = () => {
   const [data, setData] = useState([]);
 
-  const url = "https://swapi.co/api/people/";
+  const url = "https://corsanywhere.herokuapp.com/https://lambda-swapi.herokuapp.com/api/people/";
   useEffect(() => {
     axios
       .get(url)
@@ -19,22 +19,20 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1> React Wars </h1>{" "}
-      <section>
-        {" "}
+      <h1> React Wars </h1>
+      <section> 
         {data.map((character, index) => {
           return (
             <character className="person" key={index}>
-              <Card key={index} name={character.name} />{" "}
-              <Card
+              <CharacterCard 
                 key={index}
                 name={character.name}
                 age={character.birth_year}
-              />{" "}
+              />
             </character>
           );
-        })}{" "}
-      </section>{" "}
+        })}
+      </section>
     </div>
   );
 };
--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,12 +1,25 @@
 import React from "react";
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, CardImgOverlay
+  } from 'reactstrap';
+import './Card.css'
 
-function MakeCard(props) {
-  return (
-    <div>
-      <h1> Name: {props.name} </h1>{" "}
-      <h1> Age: {props.age} </h1>{" "}
-    </div>
-  );
-}
+function CharacterCard(props) {
 
-export default MakeCard;
+    return (
+        <div>
+          <Card inverse className='backgroundColor'>
+                <CardImg height='300px' width="20%" src='https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200' alt="Card image" />
+                <CardImgOverlay>
+                <CardTitle >Name: {props.name}</CardTitle>
+                <CardText >Age: {props.age}</CardText>
+                <CardText>
+                    <small className="text-muted"> </small>
+                </CardText>
+                </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    };
+export default CharacterCard;
