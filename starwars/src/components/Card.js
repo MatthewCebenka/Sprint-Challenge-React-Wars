a7d620f1125232bb720ba17a53f7bbfc6853ac1d
Matthew Cebenka <matthewt.cebenka@gmail.com>
2019-11-08 13:56:49 -0500

    MVP V2

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -10,7 +10,7 @@ function CharacterCard(props) {
     return (
         <div>
           <Card inverse className='backgroundColor'>
-                <CardImg height='300px' width="20%" src='https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200' alt="Card image" />
+                <CardImg height='370px' width="20%" src='https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200' alt="Card image" />
                 <CardImgOverlay>
                 <CardTitle >Name: {props.name}</CardTitle>
                 <CardText >Age: {props.age}</CardText>
