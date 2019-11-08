04d20a44351358b4737bf9f9b2a9323d52501732
Matthew Cebenka <matthewt.cebenka@gmail.com>
2019-11-08 11:54:10 -0500

    added card and made API Axios call

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import Card from './components/Card'
+import "./App.css";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [data, setData] = useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const url = "https://swapi.co/api/people/";
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(response => {
+        console.log(response.data.results);
+        setData(response.data.results);
+      })
+      .catch(error => console.log("ERROR"));
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <h1> React Wars </h1>{" "}
+      <section>
+        {" "}
+        {data.map((character, index) => {
+          return (
+            <character className="person" key={index}>
+              <Card key={index} name={character.name} />{" "}
+              <Card
+                key={index}
+                name={character.name}
+                age={character.birth_year}
+              />{" "}
+            </character>
+          );
+        })}{" "}
+      </section>{" "}
     </div>
   );
-}
+};
 
 export default App;
